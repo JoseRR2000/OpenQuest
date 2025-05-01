f37e4b46dc7e38dc27f2adbb578d3b896243f2d0
JoseRR2000 <jrobrio2901@g.educaand.es>
2025-05-01 14:24:12 +0200

    Navegacion creada

--- app/src/main/java/com/example/openquest/MainActivity.java
+++ app/src/main/java/com/example/openquest/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.openquest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        iniciarInvitado();
+    }
+
+    private void iniciarInvitado() {
+        Button botonInvitado = findViewById(R.id.btn_guest);
+
+        botonInvitado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PantallaJuego.class);
+                startActivity(intent);
+            }
         });
     }
 }
